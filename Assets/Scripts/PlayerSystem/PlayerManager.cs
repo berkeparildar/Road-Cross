aefde1ccc839a2c4b448ff22721a77d87161ca23
Berke Parıldar <94047726+berkeparildar@users.noreply.github.com>
2025-01-14 19:03:18 +0300

    Listening of swipe event

--- Assets/Scripts/PlayerSystem/PlayerManager.cs
+++ Assets/Scripts/PlayerSystem/PlayerManager.cs
@@ -1,9 +1,25 @@
+using InputSystem;
 using UnityEngine;
 
 namespace PlayerSystem
 {
     public class PlayerManager : MonoBehaviour
     {
+        [SerializeField] private PlayerMovementController _playerMovementController;
         
+        private void Awake()
+        {
+            InputManager.OnSwipe += MovePlayer;
+        }
+
+        private void OnDestroy()
+        {
+            InputManager.OnSwipe -= MovePlayer;
+        }
+
+        private void MovePlayer(SwipeDirection direction)
+        {
+            _playerMovementController.Move(direction);
+        }
     }
 }
